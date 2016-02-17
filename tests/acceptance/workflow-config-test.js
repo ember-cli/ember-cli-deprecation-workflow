c2c4b73e9142484d354217792dec166faca18290
Robert Jackson <robert.w.jackson@me.com>
2016-02-16 23:21:05 -0500

    Prevent deprecations from Ember.deprecate.

--- tests/acceptance/workflow-config-test.js
+++ tests/acceptance/workflow-config-test.js
@@ -16,7 +16,7 @@ module("workflow config", {
 
 test('deprecation silenced with string matcher', (assert) => {
   Ember.ENV.RAISE_ON_DEPRECATION = true;
-  Ember.deprecate('silence-me');
+  Ember.deprecate('silence-me', false, { until: 'forever', id: 'test' });
   assert.ok(true, 'Deprecation did not raise');
 });
 
@@ -25,14 +25,14 @@ test('deprecation logs with string matcher', (assert) => {
 
   let message = 'log-me';
   Ember.Logger.warn = function(passedMessage) {
-    assert.equal(passedMessage, 'DEPRECATION: ' + message, 'deprecation logs');
+    assert.ok(passedMessage.indexOf('DEPRECATION: ' + message) === 0, 'deprecation logs');
   };
-  Ember.deprecate(message);
+  Ember.deprecate(message, false, { until: 'forever', id: 'test' });
 });
 
 test('deprecation thrown with string matcher', (assert) => {
   assert.throws(function() {
-    Ember.deprecate('throw-me');
+    Ember.deprecate('throw-me', false, { until: 'forever', id: 'test' });
   }, 'deprecation throws');
 });
 
