503da786478a3218c86d68f430d94a49311435d0
Steve Calvert <scalvert@linkedin.com>
2021-09-13 16:27:54 -0700

    Reverting test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,3 @@
-import QUnit from 'qunit';
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
@@ -7,17 +6,3 @@ import { start } from 'ember-qunit';
 setApplication(Application.create(config.APP));
 
 start();
-
-QUnit.done(async function () {
-  let deprecations = self.deprecationWorkflow.flushDeprecations();
-
-  let response = await fetch('/report-deprecations', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(deprecations),
-  });
-
-  return response.json();
-});
