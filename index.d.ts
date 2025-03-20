6bc9d2e591644284b3f120b6df73f3cebf82739f
Edward Faulkner <edward@eaf4.com>
2025-03-20 14:58:58 -0400

    Options are optional
    Upon updating to the latest I was pleasantly surprised to find types included, but couldn't use them because they incorrectly claim the options are all mandatory.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-export default function setupDeprecationWorkflow(config: {
-  throwOnUnhandled: boolean;
-  workflow: {
+export default function setupDeprecationWorkflow(config?: {
+  throwOnUnhandled?: boolean;
+  workflow?: {
     handler: 'log' | 'silence' | 'throw';
     matchId: string;
     matchMessage: string;
