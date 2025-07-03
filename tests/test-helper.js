288f080a3f7af1f2b7ee1292cd78fc38bdd30bee
Chris Manson <chris@manson.ie>
2025-07-03 14:15:07 +0100

    fix more deprecations for Ember 6.x

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,11 @@
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
+setupEmberOnerrorValidation();
+loadTests();
 setApplication(Application.create(config.APP));
 
 start();
