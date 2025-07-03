c936c659b6b4f3e7f1d877f28737093e41f59e7a
Chris Manson <chris@manson.ie>
2025-07-03 15:46:26 +0100

    fix old ember-try scenarios

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,9 +3,18 @@ import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { loadTests } from 'ember-qunit/test-loader';
 import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+import { dependencySatisfies } from '@embroider/macros';
 
 setupEmberOnerrorValidation();
-loadTests();
+
+/**
+ * We only need to explicitly call load tests when we're on a newer ember-qunit. This check is here
+ * because we have an ember-try setup that tests ember versions that aren't supported on the newer
+ * ember-qunit so we need to downgrade ember-qunit for them too
+ */
+if (!dependencySatisfies('ember-qunit', '<9.0.0')) {
+  loadTests();
+}
 setApplication(Application.create(config.APP));
 
 start();
