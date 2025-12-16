1513dcdecbeb82e551f9fa52f6dd39a07c159b6a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-12-16 15:46:15 -0500

    Use experimental worker

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,5 @@
 import EmberApp from 'ember-strict-application-resolver';
 import EmberRouter from '@ember/routing/router';
-import { dependencySatisfies } from '@embroider/macros';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
